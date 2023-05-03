7b3258b19bb9c4c9e5118a124572ac69cca7aa8d
Just a Kartik <125666491+justakartik@users.noreply.github.com>
2023-05-03 16:44:22 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+/* 
+Author - justakartik
+
+*/
+
+function qdpack() {
+    console.log("qwaekactyl-desktop-pack")
+    return console.log("Thank's for using Qwaekactyl Desktop Pack")
+    
+
+}
+
+module.exports = { qdpack };
